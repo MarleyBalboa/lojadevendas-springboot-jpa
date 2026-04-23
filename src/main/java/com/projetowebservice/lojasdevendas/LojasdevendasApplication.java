c868e24b7a21b8aa99c0be03d62c2792c21bce62
Marlei <marleymlr@hotmail.com>
2026-04-23 10:48:39 -0300

    Project created

---
+++ src/main/java/com/projetowebservice/lojasdevendas/LojasdevendasApplication.java
@@ -0,0 +1,13 @@
+package com.projetowebservice.lojasdevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojasdevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojasdevendasApplication.class, args);
+	}
+
+}
