1d2ef0942fa05e521c2c931bceee52a65a78dbfc
Marlei <marleymlr@hotmail.com>
2026-05-05 15:03:45 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/projetowebservice/lojasdevendas/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.projetowebservice.lojasdevendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetowebservice.lojasdevendas.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
