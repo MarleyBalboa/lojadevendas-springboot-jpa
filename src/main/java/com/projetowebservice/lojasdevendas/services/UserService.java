cdc47fe386bac6f910840787aeed78e11aec4be3
Marlei <marleymlr@hotmail.com>
2026-05-19 09:06:05 -0300

    service layer, component, registration

---
+++ src/main/java/com/projetowebservice/lojasdevendas/services/UserService.java
@@ -0,0 +1,29 @@
+package com.projetowebservice.lojasdevendas.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetowebservice.lojasdevendas.entities.User;
+import com.projetowebservice.lojasdevendas.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
+
+}
