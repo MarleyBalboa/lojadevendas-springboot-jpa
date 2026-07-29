5b0107653066f01aa9443cbab24cad2db30d3f40
Marlei <marleymlr@hotmail.com>
2026-07-29 15:05:20 -0300

    Exception handling - update

--- src/main/java/com/projetowebservice/lojasdevendas/services/UserService.java
+++ src/main/java/com/projetowebservice/lojasdevendas/services/UserService.java
@@ -13,6 +13,8 @@ import com.projetowebservice.lojasdevendas.repositories.UserRepository;
 import com.projetowebservice.lojasdevendas.services.exceptions.DatabaseException;
 import com.projetowebservice.lojasdevendas.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try{
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj){
